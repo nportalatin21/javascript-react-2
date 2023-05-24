17aa561516c341d76d995e24023f3b1b0707a625
Nicole Portalatin <nportalatin@xpanxion.com>
2023-05-24 14:47:05 -0400

    Update Example 5

--- src/Student11/Ex5/Main.js
+++ src/Student11/Ex5/Main.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Main.css'
+import { NumberBuilder } from './NumberBuilder'
+
 
 export const Main = () => {
   return (
     <div>
-      TODO...
+      <NumberBuilder />
     </div>
   )
 }
