e7ce8a73e00af5d7ed48f7766720b1f408d50696
Nicole Portalatin <nportalatin@xpanxion.com>
2023-05-24 16:46:10 -0400

    Update Example 8

---
+++ src/Student11/Ex8/AddCalculator.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react';
+
+export const AddCalculator = () => {
+    let [add1, setAdd1] = useState(0);
+    let [add2, setAdd2] = useState(0);
+    let [add3, setAdd3] = useState(0);
+    //let [total, setTotal] = useState('');
+
+    const onHandleClick = () => {
+        setAdd1(++add1)
+       
+    
+     
+      };
+
+      const onHandlePush = () => {
+        setAdd2(add2=+ add2 + 2)
+     
+    
+     
+      };
+
+      const onHandleChevy = () => {
+        setAdd3(add3=+ add3 + 3)
+   
+    
+     
+      };
+    //   const onHandleReset = () => {
+    //     setAdd('');
+    //     setCounter(0)
+    
+    //   }
+    
+  return (
+    <div className='Main'>
+   
+    
+     <button onClick={() => onHandleClick(add1)}>Add 1</button>
+     <button onClick={() => onHandlePush(add2)}>Add 2</button>
+     <button onClick={() => onHandleChevy(add3)}>Add 3</button>
+
+
+    <p>Total: {(add1 + add2 + add3)} </p>
+     </div>
+
+  )
+}
